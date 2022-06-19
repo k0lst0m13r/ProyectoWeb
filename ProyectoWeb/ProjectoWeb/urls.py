5928bd5b9244d6db78ec57a0a713e9d37c55fbd4
Julio Branda <tete@L4-P0lv3r1t4>
2022-06-18 22:06:50 -0300

    cambiadas las urls a la aplicacion

--- ProyectoWeb/ProjectoWeb/urls.py
+++ ProyectoWeb/ProjectoWeb/urls.py
@@ -14,14 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from ProyectoWebApp import views
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home, name="Home"),
-    path('tienda',views.tienda, name="Tienda"),
-    path('servicios',views.servicios, name="Servicios"),
-    path('blog',views.blog, name="Blog"),
-    path('contacto',views.contacto, name="Contacto"),
+    path('ProyectoWebApp/', include('ProyectoWebApp.urls'))
 ]
---
+++ ProyectoWeb/ProyectoWebApp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from ProyectoWebApp import views
+
+
+
+urlpatterns = [
+    
+    path('',views.home, name="Home"),
+    path('tienda',views.tienda, name="Tienda"),
+    path('servicios',views.servicios, name="Servicios"),
+    path('blog',views.blog, name="Blog"),
+    path('contacto',views.contacto, name="Contacto"),
+]
